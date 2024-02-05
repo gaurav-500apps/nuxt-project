9ce9552d8510b5150998e2ace0b962795ba9b6a8
gauravfar93 <gauravfar93@gmail.com>
2024-02-05 13:10:44 +0530

    Categories selection implemented

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,35 +1,51 @@
 <script setup>
-definePageMeta({
-  layout: "products",
-  middleware: ["customauth"],
-});
-
-// Used useFetch to fetch products
-// const { data: products } = useFetch("https://fakestoreapi.com/products");
-// https://fakestoreapi.com/products/category/jewelery
-// console.log(products);
-
 const data = ref([]);
-
+const selectedCategory = ref("");
+const categories = [
+  "jewelery",
+  "electronics",
+  "men's clothing",
+  "women's clothing",
+];
 async function getData() {
   try {
-    const res = await fetch("https://fakestoreapi.com/products");
+    let apiUrl = "https://fakestoreapi.com/products";
+
+    // Append category to the API URL if selected
+    if (selectedCategory.value) {
+      apiUrl = `https://fakestoreapi.com/products/category/${selectedCategory.value}`;
+    }
+
+    const res = await fetch(apiUrl);
     const resdata = await res.json();
     data.value = resdata;
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 }
 
 onMounted(() => {
   getData();
 });
- console.log(data)
 </script>
-
 <template>
   <div>
-    <div class="text-center font-semibold mt-2 text-2xl">All Products</div>
+    <div class="text-center font-semibold mt-4 text-2xl">All Products</div>
+
+    <!-- Search input and category dropdown -->
+    <div class="flex justify-end mt-6 space-x-4">
+      <select v-model="selectedCategory" @change="getData" class="border p-1">
+        <option value="">All Categories</option>
+        <option
+          v-for="category in categories"
+          :key="category"
+          :value="category"
+        >
+          {{ category }}
+        </option>
+      </select>
+    </div>
+
     <!-- Products list -->
     <div class="grid grid-cols-4 gap-5">
       <div v-for="(product, index) in data" :key="index">
