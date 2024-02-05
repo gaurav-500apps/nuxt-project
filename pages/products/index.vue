283cfd7f0142622ca98b7eeba22384960c4b5f45
gauravfar93 <gauravfar93@gmail.com>
2024-02-05 12:19:10 +0530

    changes made in API call function

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -5,20 +5,35 @@ definePageMeta({
 });
 
 // Used useFetch to fetch products
-const { data: products } = useFetch("https://fakestoreapi.com/products");
+// const { data: products } = useFetch("https://fakestoreapi.com/products");
 // https://fakestoreapi.com/products/category/jewelery
-console.log(products);
+// console.log(products);
+
+const data = ref([]);
+
+async function getData() {
+  try {
+    const res = await fetch("https://fakestoreapi.com/products");
+    const resdata = await res.json();
+    data.value = resdata;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+onMounted(() => {
+  getData();
+});
+ console.log(data)
 </script>
 
 <template>
   <div>
     <div class="text-center font-semibold mt-2 text-2xl">All Products</div>
     <!-- Products list -->
-    <div v-if="products">
-      <div class="grid grid-cols-4 gap-5">
-        <div v-for="product in products" :key="product.id">
-          <ProductCard :product="product" />
-        </div>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="(product, index) in data" :key="index">
+        <ProductCard :product="product" />
       </div>
     </div>
   </div>
